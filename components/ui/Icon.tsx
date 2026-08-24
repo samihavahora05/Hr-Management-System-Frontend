7c55cee52170cc27bb643bf33a17bfbc2bc16cb5
Samiha Vahora <samihavahora71@gmail.com>
2026-08-24 16:15:57 +0530

    Fix duplicate CheckCircle icon declaration in Icon.tsx

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -415,10 +415,3 @@ export const Printer = (props: IconProps) => (
     <rect width="12" height="8" x="6" y="14" />
   </svg>
 );
-
-export const CheckCircle = (props: IconProps) => (
-  <svg fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
-    <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-    <polyline points="22 4 12 14.01 9 11.01" />
-  </svg>
-);
